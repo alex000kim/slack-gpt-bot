8d0494d2b3c9d7f1bf0eaef5469b27d83ac4c252
Alexander Kim <alex000kim@gmail.com>
2023-03-24 15:57:05 -0400

    add response streaming

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -9,6 +9,7 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 20
 
 app = App(token=SLACK_BOT_TOKEN)
 openai.api_key = OPENAI_API_KEY
@@ -51,18 +52,22 @@ conversations = {}
 
 @app.event("app_mention")
 def command_handler(body, say, context):
+    channel_id = body['event']['channel']
     if body['event'].get('thread_ts'):
         thread_ts = body['event']['thread_ts']
     else:
         thread_ts = body['event']['ts']
-    say("Got your request. Please wait.", thread_ts=thread_ts)
+        
+    slack_resp = app.client.chat_postMessage(
+            channel=channel_id,
+            thread_ts=thread_ts,
+            text="Got your request. Please wait."
+            )
+    message_ts = slack_resp['message']['ts']
     user_message = body['event']['text']
     print(f'user_message: {user_message}')
     try:
         bot_user_id = context['bot_user_id']
-        channel_id = body['event']['channel']
-        
-        
         conversation_id = f"{channel_id}-{thread_ts}"
         user_message = user_message.replace(f'<@{bot_user_id}>', '').strip()
         user_message = augment_user_message(user_message)
@@ -75,10 +80,29 @@ def command_handler(body, say, context):
         openai_response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=conversations[conversation_id],
-        )
-        bot_response = openai_response.choices[0].message.content
-        conversations[conversation_id].append({"role": "assistant", "content": bot_response})
-        say(bot_response, thread_ts=thread_ts)
+            stream=True)
+
+        response_text = ""
+        
+        ii = 0
+        for chunk in openai_response:
+            if chunk.choices[0].delta.get('content'):
+                ii = ii + 1
+                response_text += chunk.choices[0].delta.content
+                if ii > N_CHUNKS_TO_CONCAT_BEFORE_UPDATING:
+                    app.client.chat_update(
+                        channel=channel_id,
+                        ts=message_ts,
+                        text=response_text
+                    )
+                    ii = 0
+            elif chunk.choices[0].finish_reason == 'stop':
+                app.client.chat_update(
+                        channel=channel_id,
+                        ts=message_ts,
+                        text=response_text + "\n\n<EOM>"
+                    )
+
     except Exception as e:
         print(f"Error: {e}")
         say(f"I can't provide a response: encountered an error:\n```\n{e}\n```", thread_ts=thread_ts)
