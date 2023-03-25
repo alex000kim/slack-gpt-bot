37e83f8883654374c2091d93db22cb454d9f8f5a
Alexander Kim <alex000kim@gmail.com>
2023-03-25 18:28:39 -0400

    update with inital prompt

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -3,46 +3,46 @@ import os
 import openai
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from utils import augment_user_message
+from utils import augment_user_message, INIT_PROMPT, WAIT_MESSAGE, N_CHUNKS_TO_CONCAT_BEFORE_UPDATING
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 20
 
 app = App(token=SLACK_BOT_TOKEN)
 openai.api_key = OPENAI_API_KEY
     
-
 @app.event("app_mention")
 def command_handler(body, context):
     try:
         channel_id = body['event']['channel']
         thread_ts = body['event'].get('thread_ts', body['event']['ts'])
         bot_user_id = context['bot_user_id']
-
         slack_resp = app.client.chat_postMessage(
             channel=channel_id,
             thread_ts=thread_ts,
-            text="Got your request. Please wait."
+            text=WAIT_MESSAGE
             )
         reply_message_ts = slack_resp['message']['ts']
-
-        user_message = body['event']['text'].replace(f'<@{bot_user_id}>', '').strip()
-        user_message = augment_user_message(user_message)
-        
         conversation_history = app.client.conversations_replies(
             channel=channel_id,
             ts=thread_ts,
             inclusive=True
         )
-
+        # exclude the last message by bot about waiting
+        conversation_messages = conversation_history['messages'][:-1]
         messages = [{"role": "system", "content": "User has started a conversation."}]
-        for message in conversation_history['messages']:
+        messages = [{"role": "user", "content": INIT_PROMPT}]
+        for i, message in enumerate(conversation_messages):
+            message_text = message['text']
             role = "assistant" if message['user'] == bot_user_id else "user"
-            cond = (f'<@{bot_user_id}>' in message['text']) or (message['user'] == bot_user_id)
+            if i == len(conversation_messages) - 1:
+                # last message is the user's message augemented with contents from urls
+                message_text = augment_user_message(message_text)
+            cond = (f'<@{bot_user_id}>' in message_text) or (message['user'] == bot_user_id)
             if cond:
-                messages.append({"role": role, "content": message['text'].replace(f'<@{bot_user_id}>', '').strip()})
+                message_text = message_text.replace(f'<@{bot_user_id}>', '').strip()
+                messages.append({"role": role, "content": message_text})
             
         openai_response = openai.ChatCompletion.create(
             model="gpt-4",
@@ -69,7 +69,6 @@ def command_handler(body, context):
                     ts=reply_message_ts,
                     text=response_text
                 )
-        
     except Exception as e:
         print(f"Error: {e}")
         app.client.chat_postMessage(
