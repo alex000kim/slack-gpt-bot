3df0d6770f06ca17915f92a8cf4539f52eaee83b
Alexander Kim <alex000kim@gmail.com>
2023-03-27 23:30:48 -0400

    upd

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -3,11 +3,11 @@ import os
 import openai
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from utils import augment_user_message, WAIT_MESSAGE, N_CHUNKS_TO_CONCAT_BEFORE_UPDATING
 
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+from utils import (N_CHUNKS_TO_CONCAT_BEFORE_UPDATING, OPENAI_API_KEY,
+                   SLACK_APP_TOKEN, SLACK_BOT_TOKEN, SYSTEM_PROMPT,
+                   WAIT_MESSAGE, augment_user_message, extract_url_list,
+                   num_tokens_from_messages, update_chat)
 
 app = App(token=SLACK_BOT_TOKEN)
 openai.api_key = OPENAI_API_KEY
@@ -18,22 +18,12 @@ def command_handler(body, context):
         channel_id = body['event']['channel']
         thread_ts = body['event'].get('thread_ts', body['event']['ts'])
         bot_user_id = context['bot_user_id']
-        if context.get('retry'):
-            # Edit the existing message
-            app.client.chat_update(
-                channel=channel_id,
-                ts=context['message_ts'],
-                text=WAIT_MESSAGE,
-            )
-            reply_message_ts = context['message_ts']
-        else:
-            # Post a new message
-            slack_resp = app.client.chat_postMessage(
-                channel=channel_id,
-                thread_ts=thread_ts,
-                text=WAIT_MESSAGE
-            )
-            reply_message_ts = slack_resp['message']['ts']
+        slack_resp = app.client.chat_postMessage(
+            channel=channel_id,
+            thread_ts=thread_ts,
+            text=WAIT_MESSAGE
+        )
+        reply_message_ts = slack_resp['message']['ts']
         conversation_history = app.client.conversations_replies(
             channel=channel_id,
             ts=thread_ts,
@@ -41,25 +31,32 @@ def command_handler(body, context):
             )
         # exclude the last message by bot about waiting
         conversation_messages = conversation_history['messages'][:-1]
-        messages = [{"role": "system", "content": "User has started a conversation."}]
-        for i, message in enumerate(conversation_messages):
+        messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+        for message in conversation_messages:
             message_text = message['text']
             role = "assistant" if message['user'] == bot_user_id else "user"
-            if i == len(conversation_messages) - 1:
-                # last message is the user's message augemented with contents from urls
-                message_text = augment_user_message(message_text)
+            if role == "user":
+                url_list = extract_url_list(message_text)
+                if url_list:
+                    app.client.chat_update(
+                        channel=channel_id,
+                        ts=reply_message_ts,
+                        text='Detected URLs. Please wait while I process the content...'
+                    )
+                    message_text = augment_user_message(message_text, url_list)
             cond = (f'<@{bot_user_id}>' in message_text) or (message['user'] == bot_user_id)
             if cond:
                 message_text = message_text.replace(f'<@{bot_user_id}>', '').strip()
                 messages.append({"role": role, "content": message_text})
-        print(messages)
+        num_tokens = num_tokens_from_messages(messages)
+        print(f"Number of tokens: {num_tokens}")
+        
         openai_response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=messages,
             stream=True
         )
         
-
         response_text = ""
         ii = 0
         for chunk in openai_response:
@@ -67,59 +64,17 @@ def command_handler(body, context):
                 ii = ii + 1
                 response_text += chunk.choices[0].delta.content
                 if ii > N_CHUNKS_TO_CONCAT_BEFORE_UPDATING:
-                    app.client.chat_update(
-                        channel=channel_id,
-                        ts=reply_message_ts,
-                        text=response_text
-                    )
+                    update_chat(app, channel_id, reply_message_ts, response_text)
                     ii = 0
             elif chunk.choices[0].finish_reason == 'stop':
-                app.client.chat_update(
-                    channel=channel_id,
-                    ts=reply_message_ts,
-                    text=response_text,
-                    blocks=[
-                                {
-                                    "type": "section",
-                                    "text": {
-                                        "type": "mrkdwn",
-                                        "text": response_text
-                                    }
-                                },
-                                {
-                                    "type": "actions",
-                                    "elements": [
-                                        {
-                                            "type": "button",
-                                            "text": {
-                                                "type": "plain_text",
-                                                "text": "Retry"
-                                            },
-                                            "action_id": "retry_response"
-                                        }
-                                    ]
-                                }
-                            ]
-                )
+                update_chat(app, channel_id, reply_message_ts, response_text)
     except Exception as e:
         print(f"Error: {e}")
         app.client.chat_postMessage(
             channel=channel_id,
             thread_ts=thread_ts,
-            text=f"I can't provide a response. Encountered an error:\n```\n{e}\n```"
-            )
+            text=f"I can't provide a response. Encountered an error:\n`\n{e}\n`")
 
-@app.action("retry_response")
-def retry_response_handler(ack, respond, body, context):
-    ack()
-    channel_id = body['channel']['id']
-    message_ts = body['message']['ts']
-    thread_ts = body['message'].get('thread_ts', message_ts)
-    new_body = {'event': {'channel': channel_id, 'ts': thread_ts, 'thread_ts': thread_ts}}
-    new_context = context.copy()
-    new_context['retry'] = True
-    new_context['message_ts'] = message_ts
-    command_handler(new_body, new_context)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
+import os
 import re
 
-import requests
-from bs4 import BeautifulSoup
+import tiktoken
+from trafilatura import extract, fetch_url
+from trafilatura.settings import use_config
 
+newconfig = use_config()
+newconfig.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
+
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+SYSTEM_PROMPT = '''
+You are an AI assistant. 
+You will answer the question as truthfully as possible.
+If you're unsure of the answer, say Sorry, I don't know.
+'''
 WAIT_MESSAGE = "Got your request. Please wait."
-N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 20
+N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 50
+MAX_TOKENS = 8192
 
 
 def extract_url_list(text):
@@ -15,28 +30,52 @@ def extract_url_list(text):
     return url_list if len(url_list)>0 else None
 
 
-def extract_text_from_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for script in soup(['script', 'style']):
-                script.decompose()
-            text = ' '.join(soup.stripped_strings)
-            return text
-        else:
-            print(f"Error: Received a {response.status_code} status code.")
-            return None
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-    
-def augment_user_message(user_message):
-    url_list = extract_url_list(user_message)
-    if url_list:
-        all_url_content = ''
-        for url in url_list:
-            url_content = extract_text_from_url(url)
-            all_url_content = all_url_content + f'\nContent from {url}:\n"""\n{url_content}\n"""'
-        user_message = user_message + "\n" + all_url_content
+def augment_user_message(user_message, url_list):
+    all_url_content = ''
+    for url in url_list:
+        downloaded = fetch_url(url)
+        url_content = extract(downloaded, config=newconfig)
+        user_message = user_message.replace(f'<{url}>', '')
+        all_url_content = all_url_content + f' Contents of {url} : \n """ {url_content} """'
+    user_message = user_message + "\n" + all_url_content
     return user_message
+
+# From https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+def num_tokens_from_messages(messages, model="gpt-4"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+        return num_tokens_from_messages(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+def update_chat(app, channel_id, reply_message_ts, response_text):
+    app.client.chat_update(
+        channel=channel_id,
+        ts=reply_message_ts,
+        text=response_text
+    )
