129411ee08cc5153fa48e492fc1dd7c0dcdf53ff
Tyler Davis <tylerclaytondavis@gmail.com>
2023-05-12 13:48:32 -0600

    Updating channel lock to the new beta-slack-chatgpt-bot channel to prepare for internal beta release

--- slack_gpt_bot.py
+++ slack_gpt_bot.py
@@ -61,7 +61,7 @@ def command_handler(body, context):
 
         user = get_user_information(user_id)
 
-        if channel_id != 'C055BE20JJF': #lock to test channel for beta, currently using chris-gpt-test ID
+        if channel_id != 'C057NBLL2G4': #lock to test channel for beta
             slack_resp = app.client.chat_postMessage( 
                 channel=channel_id,
                 thread_ts=thread_ts,
